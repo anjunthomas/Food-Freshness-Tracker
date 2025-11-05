919f5b21ba0581704a9167d264760b763930ef57
anjunthomas <anjunoblethomas@gmail.com>
2025-11-05 18:26:16 -0500

    adding image assets, and routing between home page, login, dashboard, and signup

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Food-Freshness-Tracker
+
+Frontend Developers
+
+To work in react
+    cd Food-Freshness-Tracker/frontend
+    npm install
+    npm run dev
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import '../styles/Signup.css'
+
+function Signup() {
+    return (
+        <div className='signup-container'>
+            <h1>Signup Page</h1>
+        </div>
+    )
+}
+
+export default Signup
