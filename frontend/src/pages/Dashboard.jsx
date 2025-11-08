31e305395ff3792d73b27dce4cd0fb6d9d4b8b7d
Tasneem S Khan <tasneemsk500@gmail.com>
2025-11-07 19:47:08 -0500

    added paragraph

---
+++ frontend/src/pages/Dashboard.jsx
@@ -0,0 +1,11 @@
+import '../styles/Dashboard.css'
+
+function Dashboard(){
+    return (
+        <div className="dashboard-container">
+            <h1>Dashboard Page</h1>
+        </div>
+    )
+}
+
+export default Dashboard
---
+++ frontend/src/pages/Signup.jsx
@@ -0,0 +1,12 @@
+import '../styles/Signup.css'
+
+function Signup() {
+    return (
+        <div className='signup-container'>
+            <h1>Signup Page</h1>
+            <p class="intro">Hi this is Tasneem</p>
+        </div>
+    )
+}
+
+export default Signup
