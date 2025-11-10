1ea11e381ad443f3d36d511c8f9abdd48d193ae6
Tasneem S Khan <tasneemsk500@gmail.com>
2025-11-09 20:34:22 -0500

    Sign up form ui created

--- frontend/src/pages/Signup.jsx
+++ frontend/src/pages/Signup.jsx
@@ -3,8 +3,39 @@ import '../styles/Signup.css'
 function Signup() {
     return (
         <div className='signup-container'>
-            <h1>Signup Page</h1>
-            <p class="intro">Hi this is Tasneem</p>
+            <div className='form-container'>
+                <form action="#">
+                    {/* Title */}
+                    <div className='form_title'>
+                        <h1>Sign up!</h1>
+                    </div>
+
+                    {/* Form body */}
+                    
+                    <div class="user_name">
+                        <input type="text" class="form-control" placeholder="Name" name="name" aria-label="Name"/>
+                    </div>
+
+                    <div class="user_email">
+                        <input type="email" class="form-control" name="email" placeholder="Email"/> 
+                    </div>
+
+                    <div class="password">
+                        <input type="password" class="form-control" placeholder="Password"/>
+                    </div>
+
+                    <div class="c-password">
+                        <input type="password" class="form-control" placeholder="Confirm Password"/>
+                    </div>
+
+                    <div class="register">
+                        <a href="#">
+                            <button type="button" class="form-control form_button">Register</button>
+                        </a>
+                    </div>
+
+                </form>
+            </div>
         </div>
     )
 }
